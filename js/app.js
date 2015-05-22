25b7ce8f84a733cf4f93cd24316ab9c74305c79d
Igor Mogilin <mogistein@yandex.ru>
2015-05-23 02:35:51 +0300

    edit app.js

--- js/app.js
+++ js/app.js
@@ -5,9 +5,7 @@ var storeApp = angular.module('storeApp', []);
 });
 
 .controller('MainCtrl', '$scope', function ($scope) {
-	
 });
 
 .controller('ProductListCtrl', '$scope', function ($scope) {
-	
 });
